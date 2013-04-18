00f922923f438b5fbe9b118c96f3bee48b50ff0e
ludopradel <lpradel@astek.fr>
2013-04-18 11:44:04 +0200

    kata solution found in coding dojo

--- src/fr/astek/katas/anagram/Anagram.java
+++ src/fr/astek/katas/anagram/Anagram.java
@@ -1,11 +1,38 @@
 package fr.astek.katas.anagram;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Anagram {
 
-	public static List<String> of(String string) {
-		return null;
+	private static Set<String> concatSet(char c, Set<String> set) {
+		Set<String> newList = new TreeSet<String>();
+		for (String string : set) {
+			newList.add(c + string);
+		}
+		return newList;
 	}
 
+	public static List<String> of(String string) {
+
+		Set<String> result = new TreeSet<String>();
+
+		if (string.length() <= 1) {
+			result.add(string);
+		} else {
+
+			for (int i = 0; i < string.length(); i++) {
+				char extractedChar = string.charAt(i);
+
+				result.addAll(concatSet(
+						extractedChar,
+						new TreeSet<String>(of(string.replaceFirst("" + extractedChar, "")))));
+			}
+		}
+
+		return new ArrayList<String>(result);
+	}
 }
+
--- test/fr/astek/katas/anagram/AnagramTest.java
+++ test/fr/astek/katas/anagram/AnagramTest.java
@@ -1,16 +1,42 @@
 package fr.astek.katas.anagram;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 
 import org.junit.Test;
+
 
 public class AnagramTest {
 
 	@Test
 	public void testEmptyString() {
 		assertEquals(Arrays.asList(""), Anagram.of(""));
+	}
+	
+	@Test
+	public void testOneChar() {
+		assertEquals(Arrays.asList("A"), Anagram.of("A"));
+	}
+	
+	@Test
+	public void testTwoChar() {
+		assertEquals(Arrays.asList("AB", "BA"), Anagram.of("AB"));
+	}
+	
+	@Test
+	public void testThreeChar() {
+		assertEquals(Arrays.asList("ABC", "ACB", "BAC", "BCA", "CAB", "CBA"), Anagram.of("ABC"));
+	}
+	
+	@Test
+	public void testThreeCharWith2SameChar() {
+		assertEquals(Arrays.asList("AAB","ABA", "BAA"), Anagram.of("AAB"));
+	}
+	
+	@Test
+	public void testFourChar() {
+		assertEquals(24, Anagram.of("ABCD").size());
 	}
 
 }
